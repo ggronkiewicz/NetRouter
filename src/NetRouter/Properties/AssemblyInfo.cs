5669b7bb34ee7a85919901dcde2413ddd5c16e5b
ggronkiewicz <grzesiek0803@gmail.com>
2018-08-07 23:11:33 +0200

    Sign assemblies

--- src/NetRouter/Properties/AssemblyInfo.cs
+++ src/NetRouter/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fbc76950-cfe4-47ed-97fd-874e1933cbe5")]
-[assembly: InternalsVisibleTo("NetRouter.Tests")]
+[assembly: InternalsVisibleTo("NetRouter.Tests,PublicKey=002400000480000094000000060200000024000052534131000400000100010031b7119ba525f9f3db748a531be734922fc8fc0cad579fff67855bc3ff4a7865474dcba9dc65162a477870f9e8a9fb084fe40d0e376bae746470f1d33960ffed51bfb495e10f7dba5a07246a90a215fe73203bfaabf584ca518481069c86f56a0c9dd95a9cee987893b4dc42bc6f18fe287d500ccdf11c47c1a755bd0c300fda")]
